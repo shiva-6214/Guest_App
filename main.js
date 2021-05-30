14f541bb3b737f0813240b5d7cfbfc2e0fec79e2
Smart Coder <69685153+shiva-6214@users.noreply.github.com>
2021-05-30 18:05:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+name_of_people = [];
+
+function submit() {
+    var guestName = document.getElementById("txtGuestName").value;
+    name_of_people.push(guestName);
+
+    console.log(name_of_people);
+    document.getElementById("guestsDiv1").innerHTML = name_of_people;
+    document.getElementById("txtGuestName").value = "";
+}
+
+function show() {
+    var remove_commas = name_of_people.join("<br>");
+    document.getElementById("orderDiv2").innerHTML = remove_commas;
+    console.log(remove_commas);
+}
+
+function sorting() {
+    name_of_people.sort();
+
+    var remove_commas = name_of_people.join("<br>");
+
+    document.getElementById("sortList").innerHTML = remove_commas;
+}
+
+function searching() {
+    var s = document.getElementById("searchDiv").value;
+    var found = 0;
+    var j;
+
+    for (j = 0; j < name_of_people.length; j++) {
+        if (s == name_of_people[j]) {
+            found = found + 1;
+        }
+    }
+
+    document.getElementById("searchDiv").innerHTML = "name found" + found + "time/s";
+    console.log("found name" + found + "time/s");
+}
